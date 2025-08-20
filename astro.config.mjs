395263ea98180f8deaaca3ddeb5d714c9f08b418
Danila Kononov <danila.kononov.official@gmail.com>
2025-08-20 21:51:10 +0300

    add automatic sitemap and robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,7 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [
+        sitemap(),
+    ],
 });
