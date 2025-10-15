d5312957fa643ad4f75d214d099b8d3a571b14b7
Danila Kononov <danila.kononov.official@gmail.com>
2025-10-15 23:33:46 +0300

    remove useless comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
     site: "https://softwarecast.ru",
     vite: {
