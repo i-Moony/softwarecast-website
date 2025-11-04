b27af22e39eb9b77929aeca5706508ed55633ac8
Danila Kononov <danila.kononov.official@gmail.com>
2025-11-04 16:46:27 +0300

    add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
     site: "https://softwarecast.ru",
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [sitemap()],
+    integrations: [sitemap(), mdx()],
     devToolbar: {
         enabled: false,
     },
